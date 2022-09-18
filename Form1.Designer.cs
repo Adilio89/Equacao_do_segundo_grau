cafbec9b36e100621a1b86e6b7264bbd175126df
Adilio89 <88595417+Adilio89@users.noreply.github.com>
2022-09-18 13:51:37 -0300

    Adicionar arquivos de projeto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,234 @@
+﻿namespace Equação_do_segundo_grau
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txta = new System.Windows.Forms.TextBox();
+            this.txtb = new System.Windows.Forms.TextBox();
+            this.txtc = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtdelta = new System.Windows.Forms.TextBox();
+            this.txtx1 = new System.Windows.Forms.TextBox();
+            this.txtx2 = new System.Windows.Forms.TextBox();
+            this.btncalcular = new System.Windows.Forms.Button();
+            this.btnlimpar = new System.Windows.Forms.Button();
+            this.btnsair = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Noto Kufi Arabic", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(95, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(276, 40);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Equação do 2º Grau";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(46, 109);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(99, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Digite o valor de [a]";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(179, 109);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(99, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Digite o valor de [b]";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(315, 109);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(99, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Digite o valor de [c]";
+            // 
+            // txta
+            // 
+            this.txta.Location = new System.Drawing.Point(46, 125);
+            this.txta.Name = "txta";
+            this.txta.Size = new System.Drawing.Size(98, 20);
+            this.txta.TabIndex = 4;
+            // 
+            // txtb
+            // 
+            this.txtb.Location = new System.Drawing.Point(180, 125);
+            this.txtb.Name = "txtb";
+            this.txtb.Size = new System.Drawing.Size(98, 20);
+            this.txtb.TabIndex = 5;
+            // 
+            // txtc
+            // 
+            this.txtc.Location = new System.Drawing.Point(316, 125);
+            this.txtc.Name = "txtc";
+            this.txtc.Size = new System.Drawing.Size(98, 20);
+            this.txtc.TabIndex = 6;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(61, 209);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(72, 13);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Valor de delta";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(202, 209);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(60, 13);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "Valor de x1";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(338, 209);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(60, 13);
+            this.label7.TabIndex = 9;
+            this.label7.Text = "Valor de x2";
+            // 
+            // txtdelta
+            // 
+            this.txtdelta.Location = new System.Drawing.Point(46, 225);
+            this.txtdelta.Name = "txtdelta";
+            this.txtdelta.Size = new System.Drawing.Size(98, 20);
+            this.txtdelta.TabIndex = 10;
+            // 
+            // txtx1
+            // 
+            this.txtx1.Location = new System.Drawing.Point(180, 225);
+            this.txtx1.Name = "txtx1";
+            this.txtx1.Size = new System.Drawing.Size(98, 20);
+            this.txtx1.TabIndex = 11;
+            // 
+            // txtx2
+            // 
+            this.txtx2.Location = new System.Drawing.Point(318, 225);
+            this.txtx2.Name = "txtx2";
+            this.txtx2.Size = new System.Drawing.Size(98, 20);
+            this.txtx2.TabIndex = 12;
+            // 
+            // btncalcular
+            // 
+            this.btncalcular.Location = new System.Drawing.Point(49, 167);
+            this.btncalcular.Name = "btncalcular";
+            this.btncalcular.Size = new System.Drawing.Size(95, 23);
+            this.btncalcular.TabIndex = 13;
+            this.btncalcular.Text = "calcular";
+            this.btncalcular.UseVisualStyleBackColor = true;
+            this.btncalcular.Click += new System.EventHandler(this.btncalcular_Click);
+            // 
+            // btnlimpar
+            // 
+            this.btnlimpar.Location = new System.Drawing.Point(182, 167);
+            this.btnlimpar.Name = "btnlimpar";
+            this.btnlimpar.Size = new System.Drawing.Size(96, 23);
+            this.btnlimpar.TabIndex = 14;
+            this.btnlimpar.Text = "Limpar";
+            this.btnlimpar.UseVisualStyleBackColor = true;
+            this.btnlimpar.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnsair
+            // 
+            this.btnsair.Location = new System.Drawing.Point(318, 167);
+            this.btnsair.Name = "btnsair";
+            this.btnsair.Size = new System.Drawing.Size(96, 23);
+            this.btnsair.TabIndex = 15;
+            this.btnsair.Text = "Sair";
+            this.btnsair.UseVisualStyleBackColor = true;
+            this.btnsair.Click += new System.EventHandler(this.btnsair_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.ClientSize = new System.Drawing.Size(486, 316);
+            this.Controls.Add(this.btnsair);
+            this.Controls.Add(this.btnlimpar);
+            this.Controls.Add(this.btncalcular);
+            this.Controls.Add(this.txtx2);
+            this.Controls.Add(this.txtx1);
+            this.Controls.Add(this.txtdelta);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtc);
+            this.Controls.Add(this.txtb);
+            this.Controls.Add(this.txta);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txta;
+        private System.Windows.Forms.TextBox txtb;
+        private System.Windows.Forms.TextBox txtc;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtdelta;
+        private System.Windows.Forms.TextBox txtx1;
+        private System.Windows.Forms.TextBox txtx2;
+        private System.Windows.Forms.Button btncalcular;
+        private System.Windows.Forms.Button btnlimpar;
+        private System.Windows.Forms.Button btnsair;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Equação_do_segundo_grau
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btncalcular_Click(object sender, EventArgs e)
+        {
+            double a, b, c, delta, x;
+            try
+            {
+                a = Convert.ToDouble(txta.Text);
+                b = Convert.ToDouble(txtb.Text);
+                c = Convert.ToDouble(txtc.Text);
+                delta = Math.Pow(b, 2) - (4 * a * c);
+                txtdelta.Text = delta.ToString();
+                if (delta < 0)
+                {
+                    
+                }
+                else if (delta == 0)
+                {
+                    x = (-b) / (2 * a);
+                    txtx1.Text = x.ToString();
+                }
+                else
+                {
+                    x = ((-b) + Math.Sqrt(delta)) / (2 * a);
+                    txtx1.Text = x.ToString();
+                    x = ((-b) - Math.Sqrt(delta)) / (2 * a);
+                    txtx2.Text = x.ToString();
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Favor verifique os valores digitados");
+                txta.Focus();
+
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            txta.Clear();
+            txtb.Clear();
+            txtc.Clear();
+            txtdelta.Clear();
+            txtx1.Clear();
+            txtx2.Clear();
+        }
+
+        private void btnsair_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
